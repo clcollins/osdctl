431d7fa9f82bc6ecd8b64b1eb3fd2b2a186db190
Christopher Collins <collins.christopher@gmail.com>
2021-07-20 11:08:45 -1000

    Fixes kube cli credential usage
    Fixes the usage of kube cli credentials, to act on the currently
    logged-in cluster.
    
    Signed-off-by: Christopher Collins <collins.christopher@gmail.com>

--- cmd/cluster/must_gather.go
+++ cmd/cluster/must_gather.go
@@ -1,10 +1,18 @@
 package cluster
 
 import (
+	// "context"
+	"context"
 	"fmt"
 
-	k8spkg "github.com/openshift/osdctl/pkg/k8s"
+	"github.com/openshift/osdctl/pkg/k8s"
 	"github.com/spf13/cobra"
+
+	// "k8s.io/apimachinery/pkg/types"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	cmdutil "k8s.io/kubectl/pkg/cmd/util"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -13,8 +21,7 @@ import (
 type mustGatherOptions struct {
 	flags *genericclioptions.ConfigFlags
 	genericclioptions.IOStreams
-	k8sclient                 client.Client
-	k8sclusterresourcefactory k8spkg.ClusterResourceFactoryOptions
+	kubeCli client.Client
 }
 
 // newCmdMustGather implements the mustGather command
@@ -23,52 +30,64 @@ func newCmdMustGather(streams genericclioptions.IOStreams, flags *genericcliopti
 	ops := newMustGatherOptions(streams, flags)
 	mustGatherCmd := &cobra.Command{
 		Use:               "must-gather",
-		Short:             "retrieves a must-gather from the currently logged-in cluster",
+		Short:             "Retrieves a must-gather from the currently logged-in cluster",
 		Args:              cobra.NoArgs,
 		DisableAutoGenTag: true,
 		Run: func(cmd *cobra.Command, args []string) {
-			// cmdutil.CheckErr(ops.complete(cmd, args))
+			cmdutil.CheckErr(ops.complete(cmd, args))
 			cmdutil.CheckErr(ops.run())
 		},
 	}
-	ops.k8sclusterresourcefactory.AttachCobraCliFlags(mustGatherCmd)
 
 	// mustGatherCmd.Flags().StringVarP(&ops.output, "out", "o", "default", "Output format [default | json | env]")
 	// mustGatherCmd.Flags().BoolVarP(&ops.verbose, "verbose", "", false, "Verbose output")
-
-	fmt.Printf("%+v", ops)
 
 	return mustGatherCmd
 }
 
 func newMustGatherOptions(streams genericclioptions.IOStreams, flags *genericclioptions.ConfigFlags) *mustGatherOptions {
 	return &mustGatherOptions{
-		k8sclusterresourcefactory: k8spkg.ClusterResourceFactoryOptions{
-			Flags: flags,
-		},
+		flags:     flags,
 		IOStreams: streams,
 	}
 }
 
 func (o *mustGatherOptions) run() error {
-	fmt.Printf("+%v", o)
+	podList := &corev1.PodList{}
+
+	err := o.kubeCli.List(context.TODO(), podList, &client.ListOptions{Namespace: "openshift-monitoring"})
+	if err != nil {
+		return err
+	}
+
+	if len(podList.Items) == 0 {
+		return fmt.Errorf("no pods found")
+	}
+
+	fmt.Printf("POD COUNT: %d\n", len(podList.Items))
+
+	fmt.Printf("%+v\n", &podList.Items[0])
 
 	return nil
 }
 
-func (o *mustGatherOptions) complete(cmd *cobra.Command, _ []string) error {
-	fmt.Printf("%+v", o.flags.Context)
-	k8svalid, err := o.k8sclusterresourcefactory.ValidateIdentifiers()
-	if !k8svalid {
-		if err != nil {
-			return err
-		}
-	}
+type MustGather struct {
+}
 
-	// o.kubeCli, err = k8s.NewClient(o.flags)
-	// if err != nil {
-	// 	return err
-	// }
+type MustGatherList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []MustGather `json:"items"`
+}
+
+func (o *mustGatherOptions) complete(cmd *cobra.Command, _ []string) error {
+	var err error
+
+	o.kubeCli, err = k8s.NewClient(o.flags)
+	if err != nil {
+		fmt.Printf(err.Error())
+		return err
+	}
 
 	return nil
 }
